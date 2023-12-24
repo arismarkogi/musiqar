4c1af942e5a8edfa409f6ec15fa47bd76ed18104
Giorgos Chatzichristodoulou <132553598+gioxatz@users.noreply.github.com>
2023-12-24 20:57:24 +0200

    fixed overflows

--- lib/new_course_page1.dart
+++ lib/new_course_page1.dart
@@ -7,15 +7,10 @@ import 'widgets/custom_desc.dart';
 import 'new_course_page2.dart';
 
 
-
 class NewCoursePage1 extends StatelessWidget {
-
-
-
   TextEditingController coursename = TextEditingController();
   TextEditingController category = TextEditingController();
   TextEditingController description = TextEditingController();
-
 
   @override
   Widget build(BuildContext context) {
@@ -46,71 +41,71 @@ class NewCoursePage1 extends StatelessWidget {
           ),
         ],
       ),
-      body:
-      Center(
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(height: 20),
-            Text(
-              'Course name',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Color(0xFF6750A4),
-                fontSize: 20,
-                fontFamily: 'Roboto',
-                fontWeight: FontWeight.w700,
-                height: 0.10,
-                letterSpacing: 0.10,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20),
+              Text(
+                'Course name',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color(0xFF6750A4),
+                  fontSize: 20,
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w700,
+                  height: 0.10,
+                  letterSpacing: 0.10,
+                ),
               ),
-            ),
-            SizedBox(height: 30),
-            customInput('Course name', coursename, context: context),
-            SizedBox(height: 40),
-            Text(
-              'Category',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Color(0xFF6750A4),
-                fontSize: 20,
-                fontFamily: 'Roboto',
-                fontWeight: FontWeight.w700,
-                height: 0.10,
-                letterSpacing: 0.10,
+              SizedBox(height: 30),
+              customInput('Course name', coursename, context: context),
+              SizedBox(height: 40),
+              Text(
+                'Category',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color(0xFF6750A4),
+                  fontSize: 20,
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w700,
+                  height: 0.10,
+                  letterSpacing: 0.10,
+                ),
               ),
-            ),
-            SizedBox(height: 30),
-            customInput('Category', category, context: context),
-            SizedBox(height: 40),
-            Text(
-              'Short Desctription',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Color(0xFF6750A4),
-                fontSize: 20,
-                fontFamily: 'Roboto',
-                fontWeight: FontWeight.w700,
-                height: 0.10,
-                letterSpacing: 0.10,
+              SizedBox(height: 30),
+              customInput('Category', category, context: context),
+              SizedBox(height: 40),
+              Text(
+                'Short Desctription',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color(0xFF6750A4),
+                  fontSize: 20,
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w700,
+                  height: 0.10,
+                  letterSpacing: 0.10,
+                ),
               ),
-            ),
-            SizedBox(height: 30),
-            customdesc('Short Description', description),
-            SizedBox(height:70),
-            CancelButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => MenuPage()));
-              },
-              buttonText: 'Cancel',
-            ),
-            SizedBox(height:20),
-            CancelButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => NewCoursePage2()));
-              },
-              buttonText: 'Continue',
-            ),
-          ],
+              SizedBox(height: 30),
+              customdesc('Short Description', description),
+              SizedBox(height: 70),
+              CancelButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => MenuPage()));
+                },
+                buttonText: 'Cancel',
+              ),
+              SizedBox(height: 20),
+              CancelButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => NewCoursePage2()));
+                },
+                buttonText: 'Continue',
+              ),
+            ],
+          ),
         ),
       ),
     );
