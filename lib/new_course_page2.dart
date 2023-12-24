6acb67b49ce68b49d94b60727dd41bea2802c6d0
Giorgos Chatzichristodoulou <132553598+gioxatz@users.noreply.github.com>
2023-12-24 20:57:54 +0200

    fixed overflows

--- lib/new_course_page2.dart
+++ lib/new_course_page2.dart
@@ -126,7 +126,7 @@ class _NewCoursePage2State extends State<NewCoursePage2> {
 
 
 
-  @override
+@override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -141,59 +141,60 @@ class _NewCoursePage2State extends State<NewCoursePage2> {
           ),
         ),
       ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            SizedBox(height: 20),
-            Text(
-              'Add Chapters',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Color(0xFF6750A4),
-                fontSize: 20,
-                fontFamily: 'Roboto',
-                fontWeight: FontWeight.w700,
-                height: 0.10,
-                letterSpacing: 0.10,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20),
+              Text(
+                'Add Chapters',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color(0xFF6750A4),
+                  fontSize: 20,
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w700,
+                  height: 0.10,
+                  letterSpacing: 0.10,
+                ),
               ),
-            ),
-            SizedBox(height: 30),
-            //customInput('Chapter', Chapters, context),
-            //SizedBox(height: 20),
-            // Dynamically added CustomInputs
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: chapterInputs.length,
-              itemBuilder: (context, index) => Container(
-                width: 200, // Set a fixed width for the dynamically added items
-                child: chapterInputs[index],
+              SizedBox(height: 30),
+              // customInput('Chapter', Chapters, context),
+              // SizedBox(height: 20),
+              // Dynamically added CustomInputs
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: chapterInputs.length,
+                itemBuilder: (context, index) => Container(
+                  width: 200, // Set a fixed width for the dynamically added items
+                  child: chapterInputs[index],
+                ),
               ),
-            ),
-            SizedBox(height: 20),
-            AddChapter(
-              onPressed: () {
-                setState(() {
-                  TextEditingController newController = TextEditingController();
-                  chapterInputs.add(inputChapter('New Chapter', newController, context, chapterInputs.length));
-                  chaptersTitles.add(''); // Initialize an empty title for the new chapter
-                });
-              },
-              buttonText: '+      Add Chapter',
-            ),
-            SizedBox(height: 20),
-            CancelButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => NewCoursePage1()));
-              },
-              buttonText: 'Cancel',
-            ),
-          ],
+              SizedBox(height: 20),
+              AddChapter(
+                onPressed: () {
+                  setState(() {
+                    TextEditingController newController = TextEditingController();
+                    chapterInputs.add(inputChapter('New Chapter', newController, context, chapterInputs.length));
+                    chaptersTitles.add(''); // Initialize an empty title for the new chapter
+                  });
+                },
+                buttonText: '+      Add Chapter',
+              ),
+              SizedBox(height: 20),
+              CancelButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => NewCoursePage1()));
+                },
+                buttonText: 'Cancel',
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
 
 class NewCoursePage2 extends StatefulWidget {
   @override
